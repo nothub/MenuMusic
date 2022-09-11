2509394d18091f31c9baa93eb6a46562d0f47dca
hub <code@hub.lol>
2022-09-11 20:19:52 +0200

    1.19.2

--- src/main/java/hub/lol/menumusic/Mod.java
+++ src/main/java/hub/lol/menumusic/Mod.java
@@ -1,27 +1,29 @@
 package hub.lol.menumusic;
 
+import hub.lol.menumusic.mixins.accessors.AbstractSoundInstanceAccessor;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.sound.PositionedSoundInstance;
 import net.minecraft.client.sound.SoundInstance;
-import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class Mod implements ModInitializer {
-    public static final Identifier MUSIC_ID = new Identifier("menumusic", "music");
-    public static final SoundEvent MUSIC_EVENT = new SoundEvent(MUSIC_ID);
-    public static final SoundInstance MUSIC = new PositionedSoundInstance(Mod.MUSIC_EVENT.getId(), SoundCategory.MUSIC, 1.0F, 1.0F, true, 0, SoundInstance.AttenuationType.NONE, 0.0, 0.0, 0.0, true); // PositionedSoundInstance.music(Mod.MUSIC_EVENT);
-
     @Override
     public void onInitialize() {
-        Registry.register(Registry.SOUND_EVENT, MUSIC_ID, MUSIC_EVENT);
+        Identifier musicId = new Identifier("menumusic", "music");
+        SoundEvent musicEvent = new SoundEvent(musicId);
+        Registry.register(Registry.SOUND_EVENT, musicId, musicEvent);
+
+        SoundInstance music = PositionedSoundInstance.music(musicEvent);
+        ((AbstractSoundInstanceAccessor) music).setRepeat(true);
+
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             if (!(client.currentScreen instanceof TitleScreen)) return;
-            if (client.getSoundManager().isPlaying(MUSIC)) return;
-            client.getSoundManager().play(MUSIC);
+            if (client.getSoundManager().isPlaying(music)) return;
+            client.getSoundManager().play(music);
         });
     }
 }
---
+++ src/main/java/hub/lol/menumusic/mixins/accessors/AbstractSoundInstanceAccessor.java
@@ -0,0 +1,11 @@
+package hub.lol.menumusic.mixins.accessors;
+
+import net.minecraft.client.sound.AbstractSoundInstance;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(AbstractSoundInstance.class)
+public interface AbstractSoundInstanceAccessor {
+    @Accessor("repeat")
+    void setRepeat(boolean repeat);
+}
